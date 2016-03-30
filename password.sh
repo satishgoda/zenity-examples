e55ea56cc82645e3592580a636e9e7c6abdfa0f9
Andrew Steele <andrew@andrewsteele.me.uk>
2016-03-30 17:47:31 +0100

    Add example of password prompt dialog

---
+++ password.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+values=`zenity --password --username --text="Enter your username and password."`
+
+case $? in
+    0)
+        echo "Username: `echo $values | cut -d'|' -f1`"
+        echo "Password: `echo $values | cut -d'|' -f2`"
+        ;;
+    1)
+        echo "Login details invalid."
+        ;;
+    -1)
+        echo "An error occurred."
+        ;;
+esac
